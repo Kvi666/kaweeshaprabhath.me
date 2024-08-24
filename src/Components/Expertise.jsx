e959a962b7201d3479f3a6ab6f9ccf20e6ec8890
[kvi666] <[kvi69.exe@gmail.com]>
2024-08-24 11:18:57 +0530

    #

--- src/Components/Expertise.jsx
+++ src/Components/Expertise.jsx
@@ -14,11 +14,9 @@ const Expertise = () => {
   return (
     <div
       id="Expertise"
-      className="relative isolate overflow-hidden bg-gray-900 md:ml-96"
+      className="relative isolate overflow-hidden bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 px-12 md:ml-96"
     >
-      {" "}
-      {/* Add margin-left on medium screens and up */}
-      <div className="bg-gray-900 p-4 min-h-screen px-20">
+      <div className="bg-gray-900 p-4 min-h-screen">
         <div
           aria-hidden="true"
           className="absolute inset-0 h-max w-full m-auto grid grid-cols-2 -space-x-52 opacity-20"
@@ -27,147 +25,49 @@ const Expertise = () => {
           <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-indigo-600"></div>
         </div>
         <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-6">
-          <div className="md:w-2/3 lg:w-1/2 mt-12 text-gray-100">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="w-6 h-6 text-secondary"
-            >
-              <path
-                fillRule="evenodd"
-                d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
+          <div className="md:w-2/3 lg:w-1/2 my-24 text-gray-100">
             <h2 className="my-8 text-2xl font-bold text-white md:text-4xl">
-              Our Work
+              Expertise
             </h2>
             <p className="text-gray-300">
               We have built many products and some of them are below
             </p>
           </div>
 
-          <div className="flex justify-center gap-5">
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 justify-center">
+            {[
+              { src: Html_Logo, alt: "HTML Logo", label: "HTML" },
+              { src: Css_Logo, alt: "CSS Logo", label: "CSS" },
+              { src: Js_Logo, alt: "JavaScript Logo", label: "JavaScript" },
+              {
+                src: Typescript_Logo,
+                alt: "TypeScript Logo",
+                label: "TypeScript",
+              },
+              {
+                src: BootStrap_Logo,
+                alt: "Bootstrap Logo",
+                label: "Bootstrap",
+              },
+              { src: Tailwind_Logo, alt: "Tailwind Logo", label: "Tailwind" },
+              { src: React_Logo, alt: "React Logo", label: "React" },
+              { src: Php_Logo, alt: "PHP Logo", label: "PHP" },
+              { src: Laravel_Logo, alt: "Laravel Logo", label: "Laravel" },
+              { src: Figma_Logo, alt: "Figma Logo", label: "Figma" },
+            ].map((tech, index) => (
+              <div
+                key={index}
+                className="flex flex-col items-center justify-center border text-gray-600 border-gray-700 p-8 rounded-lg transform transition-transform hover:scale-105 hover:border-indigo-500 shadow-md"
+              >
                 <img
-                  src={Html_Logo}
+                  src={tech.src}
                   loading="lazy"
-                  className="w-16 h-16 "
-                  alt="html logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Css_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16 "
-                  alt="css logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Js_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16 rounded"
-                  alt="js logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Typescript_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16 "
-                  alt="typescript logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={BootStrap_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
                   className="w-16 h-16"
-                  alt="bootstrap logo"
+                  alt={tech.alt}
                 />
+                <p className="text-sm text-gray-300 mt-4">{tech.label}</p>
               </div>
-            </div>
-          </div>
-
-          <div className="flex justify-center gap-5">
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Tailwind_Logo}
-                  loading="lazy"
-                  className="w-16 h-16 "
-                  alt="tailwind logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={React_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16 "
-                  alt="react logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Php_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16 rounded"
-                  alt="php logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Laravel_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16 "
-                  alt="laravel logo"
-                />
-              </div>
-            </div>
-            <div className="mt-16 flex  divide-gray-700 border  text-gray-600 border-gray-700 p-16 rounded-lg">
-              <div className="m-auto">
-                <img
-                  src={Figma_Logo}
-                  loading="lazy"
-                  width="200"
-                  height="200"
-                  className="w-16 h-16"
-                  alt="figma logo"
-                />
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </div>
--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -117,7 +117,14 @@ const Resume = () => {
     >
       <div className="max-w-7xl mx-auto px-6 md:px-12 xl:px-20">
         <div className="md:w-2/3 lg:w-1/2 mt-12 text-gray-100">
-          <svg
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 h-max w-full m-auto grid grid-cols-2 -space-x-52 opacity-20"
+          >
+            <div className="blur-[106px] h-56 bg-gradient-to-br to-purple-400 from-blue-700"></div>
+            <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-indigo-600"></div>
+          </div>
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
@@ -128,9 +135,9 @@ const Resume = () => {
               d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.576 2.576l2.846.813a.75.75 0 010 1.442l-2.846.813a3.75 3.75 0 00-2.576 2.576l-.813 2.846a.75.75 0 01-1.442 0l-.813-2.846a3.75 3.75 0 00-2.576-2.576l-2.846-.813a.75.75 0 010-1.442l2.846-.813A3.75 3.75 0 007.466 7.89l.813-2.846A.75.75 0 019 4.5zM18 1.5a.75.75 0 01.728.568l.258 1.036c.236.94.97 1.674 1.91 1.91l1.036.258a.75.75 0 010 1.456l-1.036.258c-.94.236-1.674.97-1.91 1.91l-.258 1.036a.75.75 0 01-1.456 0l-.258-1.036a2.625 2.625 0 00-1.91-1.91l-1.036-.258a.75.75 0 010-1.456l1.036-.258a2.625 2.625 0 001.91-1.91l.258-1.036A.75.75 0 0118 1.5zM16.5 15a.75.75 0 01.712.513l.394 1.183c.15.447.5.799.948.948l1.183.395a.75.75 0 010 1.422l-1.183.395c-.447.15-.799.5-.948.948l-.395 1.183a.75.75 0 01-1.422 0l-.395-1.183a1.5 1.5 0 00-.948-.948l-1.183-.395a.75.75 0 010-1.422l1.183-.395c.447-.15.799-.5.948-.948l.395-1.183A.75.75 0 0116.5 15z"
               clipRule="evenodd"
             ></path>
-          </svg>
+          </svg> */}
           <h2 className="my-8 text-2xl font-bold text-white md:text-4xl">
-            Our Work
+            Resume
           </h2>
           <p className="text-gray-300">
             We have built many products and some of them are below
@@ -138,7 +145,7 @@ const Resume = () => {
         </div>
         <div class="grid grid-cols-1 md:grid-cols-2 p-12">
           <div>
-            <div className="flex justify-center">
+            <div className="flex justify-center px-5">
               <div className="w-full max-w-2xl flex justify-center">
                 <div className="bg-gray-800 py-10">
                   <div className="container mx-auto">
@@ -155,9 +162,9 @@ const Resume = () => {
                           2022 - 2023
                         </time>
                         <h3 className="text-lg font-semibold text-gray-200">
-                          Full Stack Development{" "}
+                          Full Stack Development
                         </h3>
-                        <p className="text-gray-400">
+                        <p className="text-gray-400 font-light text-sm text-justify">
                           Completed a Full Stack Development program at Moratuwa
                           University, covering frontend and backend
                           technologies. Gained skills in database management,
@@ -179,13 +186,13 @@ const Resume = () => {
                         <h3 className="text-lg font-semibold text-gray-200">
                           Frontend Development
                         </h3>
-                        <p className="text-gray-400">
+                        <p className="text-gray-400 font-light text-sm text-justify">
                           Finished a detailed Frontend Development course on
                           Udemy, mastering HTML, CSS, JavaScript, and React. The
                           course included practical projects to build responsive
                           and user-friendly web applications.
                         </p>
-                      </li>{" "}
+                      </li>
                       <li className="mb-10 ml-8">
                         <div className="absolute w-10 h-10 bg-gray-800 rounded-full -left-5 border-2 border-gray-600 flex items-center justify-center">
                           <img
@@ -200,7 +207,7 @@ const Resume = () => {
                         <h3 className="text-lg font-semibold text-gray-200">
                           MERN Stack
                         </h3>
-                        <p className="text-gray-400">
+                        <p className="text-gray-400 font-light text-sm text-justify">
                           Completed a MERN Stack course on Udemy, focusing on
                           MongoDB, Express.js, React, and Node.js. Acquired
                           hands-on experience in building full-featured web
@@ -215,8 +222,7 @@ const Resume = () => {
             </div>
           </div>
           <div>
-            {" "}
-            <div className="flex justify-center">
+            <div className="flex justify-center px-5">
               <div className="w-full max-w-2xl flex justify-center">
                 <div className="bg-gray-800 py-10">
                   <div className="container mx-auto">
@@ -235,7 +241,7 @@ const Resume = () => {
                         <h3 className="text-lg font-semibold text-gray-200">
                           Lead Developer
                         </h3>
-                        <p className="text-gray-400">
+                        <p className="text-gray-400 font-light text-sm text-justify">
                           Leading web development at Advirto Digital, managing a
                           team to design, develop, and deploy web solutions.
                           Ensuring project success through effective
@@ -257,7 +263,7 @@ const Resume = () => {
                         <h3 className="text-lg font-semibold text-gray-200">
                           Web Developer
                         </h3>
-                        <p className="text-gray-400">
+                        <p className="text-gray-400 font-light text-sm text-justify">
                           Working as a Web Developer at eSupport Technologies,
                           responsible for creating and maintaining client
                           websites. Collaborating on design, enhancing
@@ -279,7 +285,7 @@ const Resume = () => {
                         <h3 className="text-lg font-semibold text-gray-200">
                           Freelancer
                         </h3>
-                        <p className="text-gray-400">
+                        <p className="text-gray-400 font-light text-sm text-justify">
                           Providing freelance web development services on
                           Fiverr, specializing in custom websites and e-commerce
                           platforms. Managing projects from start to finish,
