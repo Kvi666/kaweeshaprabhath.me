f664373eba3f6899166e2823f59f510e8c5dbfde
[kvi666] <[kvi69.exe@gmail.com]>
2024-08-27 00:58:27 +0530

    #

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -4,22 +4,29 @@ import Dp from "../assets/dp.jpg";
 const About = () => {
   return (
     <section className="relative isolate py-12 md:py-24 px-6 md:px-12 lg:px-24 overflow-hidden bg-gray-800 md:ml-96">
+      <div
+        aria-hidden="true"
+        className="absolute inset-0 h-max w-full m-auto grid grid-cols-2 -space-x-52 opacity-20"
+      >
+        <div className="blur-[106px] h-56 bg-gradient-to-br to-purple-400 from-blue-700"></div>
+        <div className="blur-[106px] h-32 bg-gradient-to-r from-cyan-400 to-indigo-600"></div>
+      </div>
       <div className="w-full max-w-7xl mx-auto">
-        <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-12">
+        <div className="w-full grid grid-cols-1 lg:grid-cols-3 gap-12 items-center">
           {/* Image Column */}
           <div className="flex justify-center lg:justify-end lg:col-span-1">
             <img
-              className="w-full h-auto max-w-xs lg:max-w-sm rounded-3xl"
+              className="w-full h-auto max-w-xs lg:max-w-md xl:max-w-lg rounded-3xl"
               src={Dp}
               alt="About Me"
             />
           </div>
 
           {/* Text and Stats Column */}
-          <div className="lg:col-span-2 flex flex-col justify-center items-center lg:items-start gap-8 lg:gap-12">
+          <div className="lg:col-span-2 flex flex-col justify-center items-center lg:items-start lg:gap-4">
             {/* Heading and Paragraph */}
-            <div className="flex flex-col gap-6 text-center lg:text-left">
-              <h2 className="text-2xl font-bold text-white md:text-3xl lg:text-4xl">
+            <div className="flex flex-col text-center lg:text-left">
+              <h2 className="text-2xl font-bold text-white md:text-3xl lg:text-4xl py-2">
                 About Me
               </h2>
               <p className="text-gray-300 text-base lg:text-lg">
@@ -68,7 +75,7 @@ const About = () => {
             </div>
 
             {/* Button */}
-            <button className="mt-6 px-6 py-3 bg-indigo-600 hover:bg-indigo-800 transition-all duration-500 ease-in-out rounded-lg shadow-md flex justify-center items-center">
+            <button className="px-6 py-3 bg-indigo-600 hover:bg-indigo-800 transition-all duration-500 ease-in-out rounded-lg shadow-md flex justify-center items-center">
               <span className="text-white text-sm font-medium leading-6">
                 Get Started
               </span>
