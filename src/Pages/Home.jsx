2b2a800b69dcaa96272540669e568e9638e76a1d
[kvi666] <[kvi69.exe@gmail.com]>
2024-11-20 05:17:54 +0530

    #

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,4 @@
 import React from "react";
-import Hero from "../Components/Hero";
-import Services from "../Components/Services";
 import Projects from "../Components/Projects";
 import Resume from "../Components/Resume";
 import ContactForm from "../Components/ContactForm";
@@ -10,9 +8,7 @@ import About from "../Components/About";
 const Home = () => {
   return (
     <>
-      {/* <Hero /> */}
       <About />
-      {/* <Services /> */}
       <Expertise />
       <Resume />
       <Projects />
