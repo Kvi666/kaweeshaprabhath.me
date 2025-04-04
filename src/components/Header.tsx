b3875c57567706cd56a34a0d6fb8726930f19785
Kaweesha Uduwilaarachchi <kvi69.exe@gmail.com>
2025-04-04 10:07:04 +0530

    #

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,104 +1,184 @@
 "use client";
-import React from "react";
 
-interface Experience {
-  date: string;
-  title: string;
-  description: string;
-}
+import Link from "next/link";
+import { useState } from "react";
+import {
+  FaBars,
+  FaFacebookSquare,
+  FaGithubSquare,
+  FaLinkedin,
+  FaTimes,
+} from "react-icons/fa";
+import { FaEnvelope } from "react-icons/fa";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { BsDownload } from "react-icons/bs";
+import { IoLogoWhatsapp } from "react-icons/io";
 
-interface SectionProps {
-  title: string;
-  data: Experience[];
-}
+const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-const educationData: Experience[] = [
-  {
-    date: "January 2023 — June 2023 ",
-    title: "Full Stack Development",
-    description:
-      "Completed a Full Stack Development program at Moratuwa University, covering frontend and backend technologies. Gained skills in database management, server-side scripting, and dynamic web applications.",
-  },
-  {
-    date: "June 2023 — December 2023",
-    title: "Frontend Development",
-    description:
-      "Finished a detailed Frontend Development course on Udemy, mastering HTML, CSS, JavaScript, and React. The course included practical projects to build responsive and user-friendly web applications.",
-  },
-  {
-    date: "January 2024 — June 2024",
-    title: "MERN Stack",
-    description:
-      "Completed a MERN Stack course on Udemy, focusing on MongoDB, Express.js, React, and Node.js. Acquired hands-on experience in building full-featured web applications, including RESTful APIs and dynamic interfaces.",
-  },
-];
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
-const workExperienceData: Experience[] = [
-  {
-    date: "2023 - 2024",
-    title: "Senior Developer",
-    description:
-      "Senior Developer at Advirto Digital, leading web development projects, managing a team, and optimizing workflows to ensure high-quality, on-time delivery.",
-  },
-  {
-    date: "July 2020 - February 2022",
-    title: "Associate Software Engineer",
-    description:
-      "Working as an Associate Software Engineer at eSupport Technologies, responsible for developing and maintaining software applications. Collaborating with teams to design, implement, and optimize solutions, ensuring efficient functionality and a seamless user experience.",
-  },
-  {
-    date: "July 2020 - February 2022",
-    title: "Freelancer",
-    description:
-      "Providing freelance web development services on Fiverr, specializing in custom websites and e-commerce platforms. Managing client projects end-to-end, from requirements gathering to deployment and support.c",
-  },
-];
-
-function Section({ title, data }: SectionProps) {
   return (
-    <div className="space-y-6">
-      <div className="p-5">
-        <span className="bg-slate-200 px-4 py-2 rounded-full">
-          <span className="font-bold bg-gradient-to-r from-[#240CF2] to-[#00A9B5] bg-clip-text text-transparent">
-            {title}
-          </span>
-        </span>
-      </div>
+    <header className="bg-gray-800 text-white p-4 shadow-xl">
+      <nav className="max-w-[1200px] mx-auto flex justify-between items-center">
+        <p className="text-5xl font-black">Kvi</p>
+        <ul className="hidden lg:flex space-x-6 ">
+          <li>
+            <Link href="/" className="hover:text-gray-400">
+              About
+            </Link>
+          </li>
+          <li>
+            <Link href="/#Resume" className="hover:text-gray-400">
+              Resume
+            </Link>
+          </li>
+          <li>
+            <Link href="portfolio" className="hover:text-gray-400">
+              Portfolio
+            </Link>
+          </li>
 
-      {data.map((item, index) => (
-        <div key={index}>
-          <hr className="h-px my-8 bg-gray-500 border-0 md:w-full w-[90%] mx-auto" />
-          <div className="flex flex-col md:flex-row py-4 px-4">
-            <div className="w-full md:w-1/4">
-              <p className="text-lg text-gray-200">{item.date}</p>
-            </div>
-            <div className="w-full md:w-3/4">
-              <p className="text-xl font-semibold text-gray-200">
-                {item.title}
-              </p>
-              <p className="text-gray-400 text-base text-justify sm:text-lg mx-auto lg:mx-0">
-                {item.description}
-              </p>
-            </div>
+          <li>
+            <Link href="/#Contact" className="hover:text-gray-400">
+              Contact
+            </Link>
+          </li>
+        </ul>
+        <div className="hidden lg:flex items-center gap-10">
+          <button className="flex items-center gap-2 bg-gray-800 hover:bg-blue-700 text-white  py-2 px-5 rounded-md shadow-md border border-slate-500 transition-all duration-300">
+            <BsDownload className="text-lg" />
+            <a className="btn">
+              <p>Download CV</p>
+            </a>
+          </button>
+        </div>
+        {/* Hamburger Menu Icon (visible on mobile) */}
+        <div className="lg:hidden flex items-center" onClick={toggleMenu}>
+          {menuOpen ? (
+            <FaTimes className="text-white text-2xl" />
+          ) : (
+            <FaBars className="text-white text-2xl" />
+          )}
+        </div>
+      </nav>
+
+      {/* Mobile Sliding Sidebar - Right to Left */}
+      <div
+        className={`fixed top-0 right-0 h-full md:w-[40%] w-[80%]  bg-gray-900 border-l-2 border-gray-700 text-white transform ${
+          menuOpen ? "translate-x-0" : "translate-x-full"
+        } transition-transform duration-300 ease-in-out shadow-lg z-50 overflow-y-auto`}
+      >
+        <div className="p-4 flex justify-between items-center ">
+          <p className="text-4xl font-black">Kvi</p>
+          <FaTimes
+            className="text-white bg-gray-700 rounded-full  p-2  text-4xl"
+            onClick={toggleMenu}
+          />
+        </div>
+        <div className="p-4">
+          <ul className="space-y-5 text-gray-300 font-medium">
+            <li>
+              <Link
+                href="/"
+                className="hover:text-gray-400"
+                onClick={toggleMenu}
+              >
+                About
+              </Link>
+              <hr />
+            </li>
+            <li>
+              <Link
+                href="/#Resume"
+                className="hover:text-gray-400"
+                onClick={toggleMenu}
+              >
+                Resume
+              </Link>
+              <hr />
+            </li>
+            <li>
+              <Link
+                href="/projects"
+                className="hover:text-gray-400"
+                onClick={toggleMenu}
+              >
+                Projects
+              </Link>
+              <hr />
+            </li>
+            <li>
+              <Link
+                href="/#contact"
+                className="hover:text-gray-400"
+                onClick={toggleMenu}
+              >
+                Contact
+              </Link>
+              <hr />
+            </li>
+          </ul>
+        </div>
+        <div className="text-gray-200 space-y-5 px-5 ">
+          <p className=" font-semibold text-2xl ">Contact Info</p>
+
+          <div className="flex items-center gap-5">
+            <FaEnvelope className="text-gray-500" />
+            <p>dev@kaweeshaprabhath.me</p>
+          </div>
+
+          <div className="flex items-center gap-5">
+            <FaPhoneAlt className="text-gray-500" />
+            <p>+94 70 2585 455</p>
           </div>
         </div>
-      ))}
-    </div>
-  );
-}
-
-export default function Resume() {
-  return (
-    <div id="Resume" className="bg-gray-900 py-5 md:py-24">
-      <div className="mx-auto max-w-[1200px]">
-        <p className="text-3xl sm:text-3xl font-bold text-gray-400 md:text-5xl mb-8 px-5">
-          Resume
-        </p>
-        <div className="space-y-16">
-          <Section title="Education" data={educationData} />
-          <Section title="Work Experience" data={workExperienceData} />
+        <div className="p-5">
+          <button className="bg-gray-700 py-3 px-5 rounded-full">
+            GET A QUOTE
+          </button>
+          <div className="flex items-center gap-2 text-gray-500 mt-4 sm:mt-0  ">
+            <a
+              href="https://www.facebook.com/kaweeshaprabhathuduwilaarachchi"
+              target="_blank"
+            >
+              <FaFacebookSquare className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/kaweesha-prabhath-989305314/"
+              target="_blank"
+            >
+              <FaLinkedin className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
+            </a>
+            <a href="https://github.com/Kvi666" target="_blank">
+              <FaGithubSquare className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
+            </a>
+            <a href="https://www.instagram.com/kvi69.exe/" target="_blank">
+              <FaInstagram className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
+            </a>
+            <a
+              href="https://api.whatsapp.com/send?phone=94702585455"
+              target="_blank"
+            >
+              <IoLogoWhatsapp className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* Overlay Background When Menu is Open */}
+      {menuOpen && (
+        <div
+          className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-40"
+          onClick={toggleMenu}
+        ></div>
+      )}
+    </header>
   );
-}
+};
+
+export default Header;
