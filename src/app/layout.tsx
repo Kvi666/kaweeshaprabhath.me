1ae13d0578941ad547c155ebe00c6c1bc3a5c1ea
Kaweesha Uduwilaarachchi <kvi69.exe@gmail.com>
2025-04-06 23:05:36 +0530

    #

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,17 @@ import "@/app/globals.css";
 import Header from "@/components/Header";
 import Cursor from "@/components/Cursor";
 import Footer from "@/components/Footer";
+import { Kanit } from "next/font/google";
 
+const kanit = Kanit({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+  variable: "--font-kanit", // Optional: You can use this to apply the font via CSS variables
+});
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={kanit.className}>
         <Cursor />
         <div className="fixed w-full z-50">
           <Header />
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,36 @@
 "use client";
 
 import Link from "next/link";
-import { useState } from "react";
-import {
-  FaBars,
-  FaFacebookSquare,
-  FaGithubSquare,
-  FaLinkedin,
-  FaTimes,
-} from "react-icons/fa";
-import { FaEnvelope } from "react-icons/fa";
-import { FaPhoneAlt } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 import { BsDownload } from "react-icons/bs";
-import { IoLogoWhatsapp } from "react-icons/io";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [activeSection, setActiveSection] = useState<string>("");
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setMenuOpen((prev) => !prev);
   };
+
+  const handleLinkClick = (id: string) => {
+    setActiveSection(id);
+    setMenuOpen(false);
+  };
+
+  useEffect(() => {
+    if (menuOpen) {
+      const hash = window.location.hash.replace("#", "");
+      setActiveSection(hash || "About");
+    }
+  }, [menuOpen]);
 
   return (
     <header className="bg-gray-800 text-white p-4 shadow-xl">
       <nav className="max-w-[1200px] mx-auto flex justify-between items-center">
         <p className="text-5xl font-black">Kvi</p>
-        <ul className="hidden lg:flex space-x-6 ">
+        <ul className="hidden lg:flex space-x-6">
           <li>
             <Link href="/" className="hover:text-gray-400">
               About
@@ -38,11 +42,10 @@ const Header = () => {
             </Link>
           </li>
           <li>
-            <Link href="portfolio" className="hover:text-gray-400">
+            <Link href="/portfolio" className="hover:text-gray-400">
               Portfolio
             </Link>
           </li>
-
           <li>
             <Link href="/#Contact" className="hover:text-gray-400">
               Contact
@@ -50,14 +53,17 @@ const Header = () => {
           </li>
         </ul>
         <div className="hidden lg:flex items-center gap-10">
-          <button className="flex items-center gap-2 bg-gray-800 hover:bg-blue-600 text-white  py-2 px-5 rounded-md shadow-md border border-slate-500 transition-all duration-300">
+          <a
+            href="/cv.pdf"
+            download
+            className="flex items-center gap-2 bg-gray-800 hover:bg-blue-600 text-white py-2 px-5 rounded-md shadow-md border border-slate-500 transition-all duration-300"
+          >
             <BsDownload className="text-lg" />
-            <a className="btn">
-              <p>Download CV</p>
-            </a>
-          </button>
+            <p>Download CV</p>
+          </a>
         </div>
-        {/* Hamburger Menu Icon (visible on mobile) */}
+
+        {/* Hamburger */}
         <div className="lg:hidden flex items-center" onClick={toggleMenu}>
           {menuOpen ? (
             <FaTimes className="text-white text-2xl" />
@@ -67,110 +73,97 @@ const Header = () => {
         </div>
       </nav>
 
-      {/* Mobile Sliding Sidebar - Right to Left */}
+      {/* Sliding Sidebar */}
       <div
-        className={`fixed top-0 right-0 h-full md:w-[40%] w-[80%]  bg-gray-900 border-l-2 border-gray-700 text-white transform ${
+        className={`fixed top-0 right-0 h-full md:w-[40%] w-[80%] bg-gray-900 border-l-2 border-gray-700 text-white transform ${
           menuOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform duration-300 ease-in-out shadow-lg z-50 overflow-y-auto`}
       >
-        <div className="p-4 flex justify-between items-center ">
-          <p className="text-4xl font-black">Kvi</p>
+        <div className="p-4 flex justify-between items-center">
           <FaTimes
-            className="text-white bg-gray-700 rounded-full  p-2  text-4xl"
+            className="text-white bg-gray-700 rounded-full p-2 text-4xl"
             onClick={toggleMenu}
           />
         </div>
-        <div className="p-4">
-          <ul className="space-y-5 text-gray-300 font-medium text-center">
-            <li>
-              <Link
-                href="/"
-                className="hover:text-gray-400"
-                onClick={toggleMenu}
-              >
-                About
-              </Link>
-              <div className="w-full h-[2px] bg-gradient-to-r from-transparent via-blue-700 to-transparent " />
-            </li>
-            <li>
-              <Link
-                href="/#Resume"
-                className="hover:text-gray-400"
-                onClick={toggleMenu}
-              >
-                Resume
-              </Link>
-              <div className="w-full h-[2px] bg-gradient-to-r from-transparent via-blue-700 to-transparent " />
-            </li>
-            <li>
-              <Link
-                href="/portfolio"
-                className="hover:text-gray-400"
-                onClick={toggleMenu}
-              >
-                Projects
-              </Link>
-              <div className="w-full h-[2px] bg-gradient-to-r from-transparent via-blue-700 to-transparent " />
-            </li>
-            <li>
-              <Link
-                href="/#contact"
-                className="hover:text-gray-400"
-                onClick={toggleMenu}
-              >
-                Contact
-              </Link>
-              <div className="w-full h-[2px] bg-gradient-to-r from-transparent via-blue-700 to-transparent " />
-            </li>
-          </ul>
-        </div>
-        <div className="text-gray-200 space-y-5 px-5 ">
-          <p className=" font-semibold text-2xl ">Contact Info</p>
 
-          <div className="flex items-center gap-5">
-            <FaEnvelope className="text-gray-500" />
-            <p>dev@kaweeshaprabhath.me</p>
-          </div>
-
-          <div className="flex items-center gap-5">
-            <FaPhoneAlt className="text-gray-500" />
-            <p>+94 70 2585 455</p>
-          </div>
+        <div className="flex flex-col items-center mt-6 -mx-2">
+          <Image
+            className="object-cover w-28 h-28 mx-2 rounded-full"
+            src="/dp.webp"
+            alt="Profile"
+            width={112}
+            height={112}
+          />
+          <h4 className="mx-2 mt-2 font-medium text-gray-200">
+            Kaweesha Prabhath
+          </h4>
         </div>
-        <div className="p-5">
-          <button className="bg-gray-700 py-3 px-5 rounded-full">
-            GET A QUOTE
-          </button>
-          <div className="flex items-center gap-2 text-gray-500 mt-4 sm:mt-0  ">
-            <a
-              href="https://www.facebook.com/kaweeshaprabhathuduwilaarachchi"
-              target="_blank"
-            >
-              <FaFacebookSquare className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
-            </a>
-            <a
-              href="https://www.linkedin.com/in/kaweesha-prabhath-989305314/"
-              target="_blank"
-            >
-              <FaLinkedin className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
-            </a>
-            <a href="https://github.com/Kvi666" target="_blank">
-              <FaGithubSquare className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
-            </a>
-            <a href="https://www.instagram.com/kvi69.exe/" target="_blank">
-              <FaInstagram className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
-            </a>
-            <a
-              href="https://api.whatsapp.com/send?phone=94702585455"
-              target="_blank"
-            >
-              <IoLogoWhatsapp className="text-2xl md:text-3xl cursor-pointer hover:text-blue-500 transition duration-300" />
-            </a>
-          </div>
+
+        <div className="flex flex-col justify-between flex-1 mt-6">
+          <nav className="flex justify-center">
+            <ul className="space-y-2 font-medium">
+              <li>
+                <Link
+                  href="/"
+                  onClick={() => handleLinkClick("About")}
+                  className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-300 ease-in-out ${
+                    activeSection === "About"
+                      ? "bg-gray-100 dark:bg-gray-700 scale-105 shadow-lg"
+                      : ""
+                  }`}
+                >
+                  <span className="flex-1 px-3 whitespace-nowrap text-center">
+                    About
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/#Resume"
+                  onClick={() => handleLinkClick("Resume")}
+                  className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-300 ease-in-out ${
+                    activeSection === "Resume"
+                      ? "bg-gray-100 dark:bg-gray-700 scale-105 shadow-lg"
+                      : ""
+                  }`}
+                >
+                  <span className="flex-1 px-3 whitespace-nowrap text-center">
+                    Resume
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/portfolio"
+                  onClick={() => handleLinkClick("Portfolio")}
+                  className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-300 ease-in-out"
+                >
+                  <span className="flex-1 px-3 whitespace-nowrap text-center">
+                    Portfolio
+                  </span>
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/#Contact"
+                  onClick={() => handleLinkClick("Contact")}
+                  className={`flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group transition duration-300 ease-in-out ${
+                    activeSection === "Contact"
+                      ? "bg-gray-100 dark:bg-gray-700 scale-105 shadow-lg"
+                      : ""
+                  }`}
+                >
+                  <span className="flex-1 px-3 whitespace-nowrap text-center">
+                    Contact
+                  </span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
         </div>
       </div>
 
-      {/* Overlay Background When Menu is Open */}
+      {/* Background Overlay */}
       {menuOpen && (
         <div
           className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-40"
